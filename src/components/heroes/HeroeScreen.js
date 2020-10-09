4460d09b22ea7139720dedec18f985e0ea718d99
FranCoder <francoder.web@gmail.com>
2020-10-09 16:32:51 -0300

    src images

--- src/components/heroes/HeroeScreen.js
+++ src/components/heroes/HeroeScreen.js
@@ -27,7 +27,7 @@ export const HeroeScreen = ({ history }) => {
     <div className='row mt-5'>
       <div className='col-sm-12 col-md-4'>
         <img
-          src={`../assets/heroes/${heroeId}.jpg`}
+          src={`./heroes-app/assets/heroes/${heroeId}.jpg`}
           alt={superhero}
           className='img-thumbnail animate__animated animate__fadeInLeft'
         />
