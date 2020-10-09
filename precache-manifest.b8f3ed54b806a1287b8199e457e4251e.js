7b9836dea7865062c752bba15074a64a1801c236
FranCoder <francoder.web@gmail.com>
2020-10-09 16:26:57 -0300

    Updates

---
+++ precache-manifest.b8f3ed54b806a1287b8199e457e4251e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bfdbe2a14828fd941986877f272faf35",
+    "url": "/heroes-app/index.html"
+  },
+  {
+    "revision": "f036c8aaf549588ff1e1",
+    "url": "/heroes-app/static/js/2.a0f375b1.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/heroes-app/static/js/2.a0f375b1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "56b6c97889303cb7dbe4",
+    "url": "/heroes-app/static/js/main.f800b3f5.chunk.js"
+  },
+  {
+    "revision": "90266427953f7adc0e92",
+    "url": "/heroes-app/static/js/runtime-main.bf243530.js"
+  }
+]);
