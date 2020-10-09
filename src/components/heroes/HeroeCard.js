acdf33dfc2df1cd280a3841b3eb6f69f073b8877
FranCoder <francoder.web@gmail.com>
2020-10-09 16:36:24 -0300

    src img

--- src/components/heroes/HeroeCard.js
+++ src/components/heroes/HeroeCard.js
@@ -13,7 +13,7 @@ export const HeroeCard = ({
       <div className='row no-gutters'>
         <div className='col-md-4'>
           <img
-            src={`./assest/heroes/${id}.jpg`}
+            src={`./heroes-app/assest/heroes/${id}.jpg`}
             alt={superhero}
             className='card-img'
           />
